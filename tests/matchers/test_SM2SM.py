e22fdd51421752f0402145aa5153f45791d89c39
levi <levi>
2015-01-22 19:10:38 +0000

    test script

--- tests/matchers/test_SM2SM.py
+++ tests/matchers/test_SM2SM.py
@@ -10,8 +10,7 @@ Created on 2015-01-19
 '''
 import unittest
 
-from patterns.HSM2SM_matchLHS import HSM2SM_matchLHS
-from patterns.HSM2SM_rewriter import HSM2SM_rewriter
+from PyRamify import PyRamify
 
 from t_core.messages import Packet
 from t_core.iterator import Iterator
@@ -28,18 +27,29 @@ class Test(unittest.TestCase):
     def testName(self):       
         i = Iterator()
         p = Packet()
+
+        pyramify = PyRamify()
+
+        [self.rules, self.ruleTraceCheckers, backwardPatterns2Rules, backwardPatternsComplete, self.matchRulePatterns, self.ruleCombinators] = \
+            pyramify.ramify_directory("./dir_for_pyramify/", True)
+
+        HSM2SM_py = self.matchRulePatterns["HSM2SM"]
+        print(HSM2SM_py)
+
+        matcher = HSM2SM_py[0]
+        rewriter = HSM2SM_py[1]
+        p.graph = matcher.condition
+
         
-        p.graph = HSM2SM()
+        graph_to_dot("test_before_SM2SM", HSM2SM())
         
-        graph_to_dot("before_SM2SM", p.graph)
+        graph_to_dot("test_SM2SM", p.graph)
         
-        graph_to_dot("SM2SM", p.graph)
-        
-        s2s_match = Matcher(HSM2SM_matchLHS())
-        s2s_rewrite = Rewriter(HSM2SM_rewriter())
+        s2s_match = matcher
+        s2s_rewrite = rewriter
  
-        graph_to_dot("SM2SM_matcher", HSM2SM_matchLHS())
-        graph_to_dot("SM2SM_rewriter", HSM2SM_rewriter())        
+        graph_to_dot("test_SM2SM_matcher", matcher.condition)
+        graph_to_dot("test_SM2SM_rewriter", rewriter.condition)
          
         s2s_match.packet_in(p)
          
