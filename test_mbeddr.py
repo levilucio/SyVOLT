34e0bc07d2ebf4fd60d6066da0588741d4f78216
boakes <boakes>
2015-02-19 03:23:10 +0000

    Fix crash.

--- test_mbeddr.py
+++ test_mbeddr.py
@@ -96,7 +96,8 @@ class Test():
         d3 = self.rules['Hlayer3rule3']
         d4 = self.rules['Hlayer3rule4']
         d5 = self.rules['Hlayer3rule5']
-            
+
+        expected_num_pcs = args.num_pcs
         transformation = [[a0,a1,a2,a3,a4,a5,a6],[b3,b4,b5,b6,b7,b8],[c0,c1,c2,c3],[d0,d1,d2,d3,d4,d5]]
           
         pre_metamodel = ["MT_pre__mbeddr_MM", "MoTifRule"]
