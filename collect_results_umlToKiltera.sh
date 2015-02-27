6d3ca837c1ab630b36db0b69268580cfb4ce2e19
boakes <boakes>
2015-02-27 22:55:48 +0000

    Fix results scripts

--- collect_results_umlToKiltera.sh
+++ collect_results_umlToKiltera.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
-echo "Testing test_umlToKiltera.py" > results.txt
-for i in {1..15}
+echo "Testing test_umlToKiltera.py" > results_umlToKiltera.txt
+for i in {1..2}
 do
-   echo -e "\nNumber of rules: $i" >> results.txt
+   echo -e "\nNumber of rules: $i" >> results_umlToKiltera.txt
    /usr/bin/time python2 test_umlToKiltera.py --no_svg --num_rules=$i  2>&1 | tail -4 >> results_umlToKiltera.txt
 done
