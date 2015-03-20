4f1c4c238554a55b3f4f02652111cce3bd10bcef
boakes <boakes>
2015-03-20 20:17:21 +0000

    Remove more Cython files, and start with parallelization work.

---
+++ PCDict.py
@@ -0,0 +1,35 @@
+import cPickle as pickle
+import gzip
+import os
+import sys
+import collections
+
+
+from core.himesis_utils import shrink_graph, expand_graph
+
+class PCDict(object):
+
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.cache = collections.OrderedDict()
+        self.pcdict = {}
+
+    def __getitem__(self, key):
+        try:
+            return self.cache[key]
+        except KeyError:
+            value = expand_graph(self.pcdict[key])
+
+            self.__setitem__(key, value)
+            return value
+
+    def __setitem__(self, key, value):
+        self.cache[key] = value
+
+        if len(self.cache) > self.capacity:
+
+            (key, value) = self.cache.popitem(last=False)
+
+            small_value = shrink_graph(value)
+            self.pcdict[key] = small_value
---
+++ t_core/rewriter.py
@@ -0,0 +1,64 @@
+
+from rule_primitive import RulePrimitive
+from messages import TransformationException
+from core.himesis import Himesis
+
+import traceback
+
+class Rewriter(RulePrimitive):
+    '''
+        Transforms the matched source model elements according to the specified post-condition pattern.
+    '''
+    def __init__(self, condition):
+        '''
+            Transforms the bound graph of the source graph into what the specification of the post-condition pattern.
+            @param condition: The the post-condition pattern.
+        '''
+        super(Rewriter, self).__init__()
+        self.condition = condition
+    
+    def __str__(self):
+        s = super(Rewriter, self).__str__()
+        s = s.split(' ')
+        s.insert(1, '[%s]' % self.condition.name)
+        return reduce(lambda x, y: x + ' ' + y, s)
+    
+    def packet_in(self, packet):
+        self.exception = None
+        self.is_success = False
+        if self.condition.pre[Himesis.Constants.GUID] not in packet.match_sets:
+            self.is_success = False
+            # TODO: This should be a TransformationLanguageSpecificException 
+            self.exception = TransformationException()
+            self.exception.packet = packet
+            return packet
+        else:
+            match = packet.match_sets[self.condition.pre[Himesis.Constants.GUID]].match2rewrite
+            try:
+                mapping = match.to_label_mapping(packet.graph)
+                # Apply the transformation on the match
+
+                self.condition.execute(packet, mapping)     # Sets dirty nodes as well
+            except Exception, e:
+
+                tb = traceback.format_exc()
+                print("Rewriter Error: " + str(e))
+                print(tb)
+                print("packet.graph: " + packet.graph.name)
+                print("self.condition: " + self.condition.name)
+                raise
+                self.is_success = False
+                self.exception = TransformationException(e)
+                self.exception.packet = packet
+                self.exception.transformation_unit = self
+                return packet
+            
+            # Remove the match
+            packet.match_sets[self.condition.pre[Himesis.Constants.GUID]].match2rewrite = None
+            if  len(packet.match_sets[self.condition.pre[Himesis.Constants.GUID]].matches) == 0:
+                del packet.match_sets[self.condition.pre[Himesis.Constants.GUID]]
+            
+            #print self.condition
+            
+            self.is_success = True
+            return packet
