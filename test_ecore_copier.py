162c9c79f573b4ec187fa09b9a9e4d09069ca143
Bentley James Oakes <bentleyjoakes@gmail.com>
2015-08-11 22:56:06 -0400

    Working on collapsing all ecore copier tests into one.

--- test_ecore_copier.py
+++ test_ecore_copier.py
@@ -21,36 +21,36 @@ from core.himesis_utils import graph_to_dot
 from ecore_utils import EcoreUtils
 from core.himesis_plus import buildPreListFromClassNames
 
+from util.test_script_utils import select_rules, slice_transformation
+
 # all runs are the same transformation, but with different metamodel elements
 # the purpose is to do scalability testing with multiple configurations and multiple sets of rules
 
 
-# from PropertyVerification.state_property import StateProperty
-# from PropertyVerification.atomic_state_property import AtomicStateProperty
-# from PropertyVerification.and_state_property import AndStateProperty
-# from PropertyVerification.or_state_property import OrStateProperty
-# from PropertyVerification.not_state_property import NotStateProperty
-# from PropertyVerification.implication_state_property import ImplicationStateProperty
-# from PropertyVerification.Not import Not #StateSpace Prop
-# from PropertyVerification.Implication import Implication #StateSpace Prop
-# from PropertyVerification.And import And #StateSpace Prop
-# from PropertyVerification.Or import Or #StateSpace Prop
-# from PropertyVerification.BACKUP_atomic_state_property import BKUPAtomicStateProperty
-# #from lib2to3.fixer_util import p1
-#
-# from PropertyVerification.PropertyVerifier import PropertyVerifier
-#
-#
-# #positive
-#
-#
-# from ECore_Copier_MM.properties.positive.himesis.HProperty1_isolatedLHS import HProperty1_isolatedLHS
-# from ECore_Copier_MM.properties.positive.himesis.HProperty1_connectedLHS import HProperty1_connectedLHS
-# from ECore_Copier_MM.properties.positive.himesis.HProperty1_completeLHS import HProperty1_completeLHS
-#
-# from ECore_Copier_MM.properties.positive.himesis.HProperty2_isolatedLHS import HProperty2_isolatedLHS
-# from ECore_Copier_MM.properties.positive.himesis.HProperty2_connectedLHS import HProperty2_connectedLHS
-# from ECore_Copier_MM.properties.positive.himesis.HProperty2_completeLHS import HProperty2_completeLHS
+from PropertyVerification.state_property import StateProperty
+from PropertyVerification.atomic_state_property import AtomicStateProperty
+from PropertyVerification.and_state_property import AndStateProperty
+from PropertyVerification.or_state_property import OrStateProperty
+from PropertyVerification.not_state_property import NotStateProperty
+from PropertyVerification.implication_state_property import ImplicationStateProperty
+from PropertyVerification.Not import Not #StateSpace Prop
+from PropertyVerification.Implication import Implication #StateSpace Prop
+from PropertyVerification.And import And #StateSpace Prop
+from PropertyVerification.Or import Or #StateSpace Prop
+
+from PropertyVerification.PropertyVerifier import PropertyVerifier
+
+
+#positive
+
+
+from ECore_Copier_MM.properties.positive.himesis.HProperty1_isolatedLHS import HProperty1_isolatedLHS
+from ECore_Copier_MM.properties.positive.himesis.HProperty1_connectedLHS import HProperty1_connectedLHS
+from ECore_Copier_MM.properties.positive.himesis.HProperty1_completeLHS import HProperty1_completeLHS
+
+from ECore_Copier_MM.properties.positive.himesis.HProperty2_isolatedLHS import HProperty2_isolatedLHS
+from ECore_Copier_MM.properties.positive.himesis.HProperty2_connectedLHS import HProperty2_connectedLHS
+from ECore_Copier_MM.properties.positive.himesis.HProperty2_completeLHS import HProperty2_completeLHS
 
 
 
@@ -61,88 +61,138 @@ class Test():
 
     def setUp(self, args):
         pyramify = PyRamify(verbosity = args.verbosity, draw_svg=args.draw_svg)
-        
-        self.rules = pyramify.get_rules("ECore_Copier_MM/transformation/")
+
+        # full_transformation = [
+        #     ['HEAttribute'],
+        #
+        #     ['HEClass',
+        #     'HEDataType'],
+        #
+        #     ['HEEnum',
+        #     'HEEnumLiteral'],
+        #
+        #     ['HEFactory',
+        #     'HEOperation',
+        #      'HEAnnotation'],
+        #
+        #     ['HEPackage',
+        #     'HEParameter'],
+        #
+        #     ['HEReference',
+        #     'HEStringToStringMapEntry'],
+        #
+        #     ['HeattributelefteTypeSolveRefEAttributeEClassifierEAttributeEClassifier'],
+        #
+        #     ['HeclasslefteOperationsSolveRefEClassEOperationEClassEOperation'],
+        #     #        'HeclasslefteSuperTypesSolveRefEClassEClassEClassEClass']
+        #
+        #     ['HeenumlefteLiteralsSolveRefEEnumEEnumLiteralEEnumEEnumLiteral',
+        #     'HefactoryleftePackageSolveRefEFactoryEPackageEFactoryEPackage'],
+        #
+        #     ['HeoperationlefteParametersSolveRefEOperationEParameterEOperationEParameter',
+        #     'HeoperationlefteTypeSolveRefEOperationEClassifierEOperationEClassifier'],
+        #
+        #     ['HepackagelefteFactoryInstanceSolveRefEPackageEFactoryEPackageEFactory'],
+        #     #'HepackagelefteSubpackagesSolveRefEPackageEPackageEPackageEPackage']
+        #
+        #     ['HeparameterlefteTypeSolveRefEParameterEClassifierEParameterEClassifier'],
+        #     #'HereferencelefteOppositeSolveRefEReferenceEReferenceEReferenceEReference']
+        #
+        #     ['HereferencelefteTypeSolveRefEReferenceEClassifierEReferenceEClassifier',
+        #     'HeclasslefteStructuralFeaturesSolveRefEClassEStructuralFeatureEClassEStructuralFeature'],
+        #
+        #     ['HeattributelefteAnnotationsSolveRefEAttributeEAnnotationEAttributeEAnnotation'
+        #     'HedatatypelefteAnnotationsSolveRefEDataTypeEAnnotationEDataTypeEAnnotation'],
+        #
+        #     ['HeenumlefteAnnotationsSolveRefEEnumEAnnotationEEnumEAnnotation'],
+        #
+        #
+        #     ['HeoperationlefteExceptionsSolveRefEOperationEClassifierEOperationEClassifier'],
+        #
+        #     ['HepackagelefteAnnotationsSolveRefEPackageEAnnotationEPackageEAnnotation'],
+        #     ['HepackagelefteClassifiersSolveRefEPackageEClassifierEPackageEClassifier'],
+        #
+        #     ['HereferencelefteAnnotationsSolveRefEReferenceEAnnotationEReferenceEAnnotation'],
+        #
+        #     ['HereferencelefteKeysSolveRefEReferenceEAttributeEReferenceEAttribute']
+        # ]
+
+        full_transformation = [
+            ['HEClass'],
+            ['HEClass_Copy'],
+
+            ['HEReference'],
+            ['HEReference_Copy'],
+
+            ['HeclassOUTeAnnotationsSolveRefEClassEAnnotationEClassEAnnotation'],
+            ['HeclassOUTeTypeParametersSolveRefEClassETypeParameterEClassETypeParameter'],
+
+            ['HeclassOUTeSuperTypesSolveRefEClassEClassEClassEClass'],
+            ['HeclassOUTeOperationsSolveRefEClassEOperationEClassEOperation'],
+
+            ['HeclassOUTeStructuralFeaturesSolveRefEClassEStructuralFeatureEClassEStructuralFeature'],
+            ['HeclassOUTeGenericSuperTypesSolveRefEClassEGenericTypeEClassEGenericType'],
+
+            ['HereferenceOUTeAnnotationsSolveRefEReferenceEAnnotationEReferenceEAnnotation'],
+            ['HereferenceOUTeTypeSolveRefEReferenceEClassifierEReferenceEClassifier'],
+
+            ['HereferenceOUTeGenericTypeSolveRefEReferenceEGenericTypeEReferenceEGenericType'],
+            ['HereferenceOUTeOppositeSolveRefEReferenceEReferenceEReferenceEReference'],
+
+            ['HereferenceOUTeKeysSolveRefEReferenceEAttributeEReferenceEAttribute'],
+
+            ['HeattributeOUTeAnnotationsSolveRefEAttributeEAnnotationEAttributeEAnnotation'],
+            ['HeattributeOUTeTypeSolveRefEAttributeEClassifierEAttributeEClassifier'],
+
+            ['HeattributeOUTeGenericTypeSolveRefEAttributeEGenericTypeEAttributeEGenericType'],
+
+            ['HEAttribute']
+        ]
+
+
+
+        # if self.slice_for_prop1:
+        #     transformation = [[a1], [a1_2], [b1], [b1_2], [c1], [d1], [e1], [f1], [g1], [h1], [i1], [j1], [k1], [l1], [m1]]
+        #     # transformation = [[a1], [a1_2], [b1],  [g1]]
+        # else:
+        #     transformation = [[a1], [b1], [q1], [c1], [d1], [e1], [f1], [g1], [h1], [i1], [j1], [k1], [l1], [m1], [n1], [o1],
+        #                       [p1]]
+
+
+
+        self.rules, self.transformation = pyramify.get_rules("ECore_Copier_Large_MM/transformation/", full_transformation)
 
         #print("Rules: " + str(self.rules.keys()))
 
 
-    def select_rules(self, full_transformation, num_rules):
-        selected_transformation = []
+        prop1_atomic = AtomicStateProperty(HProperty1_isolatedLHS(), HProperty1_connectedLHS(), HProperty1_completeLHS())
+        prop2_atomic = AtomicStateProperty(HProperty2_isolatedLHS(), HProperty2_connectedLHS(), HProperty2_completeLHS())
 
-        print("Selecting " + str(num_rules) + " rules")
+        # graph_to_dot("prop2_iso", HProperty2_isolatedLHS())
+        # graph_to_dot("prop2_con", HProperty2_connectedLHS())
+        # graph_to_dot("prop2_comp", HProperty2_completeLHS())
 
-        i = 1
-        for layer in range(len(full_transformation)):
-            selected_transformation.append([])
-            for rule in full_transformation[layer]:
-                selected_transformation[layer].append(rule)
-                i += 1
-                if i > num_rules:
-                    print("Returning: " + str(selected_transformation))
-                    return selected_transformation
+        #
+        # #atomic_properties = [["HDaughterMother_atomic", HDaughterMother_atomic]]
+        self.atomic_properties = [["prop1", prop1_atomic], ["prop2", prop2_atomic]]
+
+        self.if_then_properties = []
+        #
+        # if_then_properties = [["HCommunityPerson", HCommunityPersonIfClause, HCommunityPersonThenClause]]
+        #
+
+        if args.slice > 0:
+            contract = self.atomic_properties[args.slice - 1]
+            self.rules, self.transformation = slice_transformation(self.rules, self.transformation, contract, args)
 
     def test_correct_ecore_copier(self,args):
 
         pyramify = PyRamify(verbosity = args.verbosity, draw_svg=args.draw_svg)
 
-        a1 = self.rules['HEAttribute']
+        [self.rules, self.ruleTraceCheckers, backwardPatterns2Rules, backwardPatternsComplete, self.matchRulePatterns,
+         self.ruleCombinators, self.overlapping_rules, self.subsumption, self.loopingRuleSubsumption] = \
+            pyramify.ramify_directory("ECore_Copier_Large_MM/transformation/", self.transformation)
 
-        b1 = self.rules['HEClass']
-        c1 = self.rules['HEDataType']
-
-        d1 = self.rules['HEEnum']
-        e1 = self.rules['HEEnumLiteral']
-
-
-        f1 = self.rules['HEFactory']
-        g1 = self.rules['HEOperation']
-
-        h1 = self.rules['HEPackage']
-        i1 = self.rules['HEParameter']
-
-        j1 = self.rules['HEReference']
-        k1 = self.rules['HEStringToStringMapEntry']
-
-        l1 = self.rules['HeattributelefteTypeSolveRefEAttributeEClassifierEAttributeEClassifier']
-
-        m1 = self.rules['HeclasslefteOperationsSolveRefEClassEOperationEClassEOperation']
-#        n1 = self.rules['HeclasslefteSuperTypesSolveRefEClassEClassEClassEClass']
-
-        o1 = self.rules['HeenumlefteLiteralsSolveRefEEnumEEnumLiteralEEnumEEnumLiteral']
-        p1 = self.rules['HefactoryleftePackageSolveRefEFactoryEPackageEFactoryEPackage']
-
-        q1 = self.rules['HeoperationlefteParametersSolveRefEOperationEParameterEOperationEParameter']
-        r1 = self.rules['HeoperationlefteTypeSolveRefEOperationEClassifierEOperationEClassifier']
-
-        s1 = self.rules['HepackagelefteFactoryInstanceSolveRefEPackageEFactoryEPackageEFactory']
-#        t1 = self.rules['HepackagelefteSubpackagesSolveRefEPackageEPackageEPackageEPackage']
-
-        u1 = self.rules['HeparameterlefteTypeSolveRefEParameterEClassifierEParameterEClassifier']
-#        v1 = self.rules['HereferencelefteOppositeSolveRefEReferenceEReferenceEReferenceEReference']
-
-        w1 = self.rules['HereferencelefteTypeSolveRefEReferenceEClassifierEReferenceEClassifier']
-        x1 = self.rules['HeclasslefteStructuralFeaturesSolveRefEClassEStructuralFeatureEClassEStructuralFeature']
-
-
-        #get the expected num from the args
-        #expected_num_pcs = args.num_pcs
-        expected_num_pcs = 330
-                
-        #TODO: Change this number if you are modifying the transformation at all
-        #if args.num_rules == -1:
-        #transformation = [[a1], [b1], [c1], [d1], [e1], [f1], [g1], [h1], [i1], [j1], [k1], [l1], [m1], [n1], [o1], [p1], [q1], [r1], [s1], [t1], [u1], [v1], [w1]]
-        transformation = [[a1], [b1], [c1], [d1], [e1], [f1], [g1], [h1], [i1], [j1], [k1], [l1], [m1], [o1], [p1], [q1], [r1], [s1], [u1], [w1], [x1]]
-        
-        [self.rules, self.ruleTraceCheckers, backwardPatterns2Rules, backwardPatternsComplete, self.matchRulePatterns, self.ruleCombinators] = \
-            pyramify.ramify_directory("ECore_Copier_MM/transformation/", transformation)
-
-        #else:
-        #    transformation = self.select_rules([[a1,a2], [b1,b2,b3], [c1,c2,c3], [d1,d2,d3], [e1,e2,e3,e4], [f1]], args.num_rules)
-
-
-        #transformation =[[a1], [ b3]]
-        #transformation =[[a1], [b1,b2,b3], [c1,c2,c3], [d1,d2,d3], [e1,e2,e3,e4], [f1]]
         pre_metamodel = ["MT_pre__S_MM", "MoTifRule"]
         post_metamodel = ["MT_post__T_MM", "MoTifRule"]
 
@@ -159,7 +209,7 @@ class Test():
         subclasses_dict["MT_pre__EStructuralFeature"] = ["MT_pre__EReference"]
             
         
-        eu2 = EcoreUtils("ECore_Copier_MM/Ecore.ecore")
+        eu2 = EcoreUtils("ECore_Copier_Large_MM/Ecore.ecore")
         subclasses_dict["MT_pre__MetaModelElement_T"] = buildPreListFromClassNames(eu2.getMetamodelClassNames())
 
 
@@ -204,7 +254,7 @@ class Test():
                 #change_subtype_matching(self.ruleTraceCheckers[tracer_key],subclasses_dict)
 
 
-        s = PathConditionGenerator(transformation, self.ruleCombinators, self.ruleTraceCheckers, self.matchRulePatterns, args)#
+        s = PathConditionGenerator(self.transformation, self.ruleCombinators, self.ruleTraceCheckers, self.matchRulePatterns, self.overlapping_rules, self.subsumption, self.loopingRuleSubsumption, args)#
    
         ts0 = time.time()
         s.build_path_conditions()
@@ -216,13 +266,6 @@ class Test():
 #        print("Size of the set of path conditions: " + str(float(sys.getsizeof(s.pathConditionSet) / 1024)))
         print("Number of path conditions: " + str(s.num_path_conditions))
 
-        #check if the correct number of path conditions were produced
-        if not int(expected_num_pcs) == -1 and not int(expected_num_pcs) == s.num_path_conditions:
-
-            #TODO: Make this an exception
-            num_pcs_s = "The number of produced path conditions is incorrect.\n" + str(expected_num_pcs) + " were expected, but " + str(s.num_path_conditions) + " were produced."
-            print(num_pcs_s)
-            #raise Exception(num_pcs_s)
 
 #         print("printing path conditions")
 #         s.print_path_conditions_screen()
@@ -238,20 +281,7 @@ class Test():
         #s.print_path_conditions_file()
 
 
-        prop1_atomic = AtomicStateProperty(HProperty1_isolatedLHS(), HProperty1_connectedLHS(), HProperty1_completeLHS())
-        prop2_atomic = AtomicStateProperty(HProperty2_isolatedLHS(), HProperty2_connectedLHS(), HProperty2_completeLHS())
-
-        #graph_to_dot("prop2_iso", HProperty2_isolatedLHS())
-        #graph_to_dot("prop2_con", HProperty2_connectedLHS())
-        #graph_to_dot("prop2_comp", HProperty2_completeLHS())
-
-        #
-        # #atomic_properties = [["HDaughterMother_atomic", HDaughterMother_atomic]]
-        atomic_properties = [["prop1", prop1_atomic], ["prop2", prop2_atomic]]
-        #
-        # if_then_properties = [["HCommunityPerson", HCommunityPersonIfClause, HCommunityPersonThenClause]]
-        #
-        #
+         #
         # #s.verbosity = 2
         #
         #
@@ -262,9 +292,11 @@ class Test():
         # #     print("Atomic property: " + "HCommunityPerson1" + " does not hold")
         #
         #
-        for name, atomic_prop in atomic_properties:
+
+        verifier = StateProperty()
+        for name, atomic_prop in self.atomic_properties:
             ts0 = time.time()
-            finalresult = StateProperty.verifyCompositeStateProperty(s, atomic_prop)
+            finalresult = verifier.verifyCompositeStateProperty(s, atomic_prop)
             ts1 = time.time()
             if len(finalresult) == 0:
                 print("Atomic property: " + name + " holds\n")
@@ -272,8 +304,8 @@ class Test():
                 print("Atomic property: " + name + " does not hold\n")
             print("Took " + str(ts1 - ts0) + " time to verify ")
         #
-        # for name, i, t in if_then_properties:
-        #     finalresult = StateProperty.verifyCompositeStateProperty(s, ImplicationStateProperty(i, t))
+        # for name, i, t in self.if_then_properties:
+        #     finalresult = verifier.verifyCompositeStateProperty(s, ImplicationStateProperty(i, t))
         #     if len(finalresult) == 0:
         #         print("If-then property: " + name + " holds\n")
         #     else:
@@ -281,7 +313,7 @@ class Test():
         #
         # ts1 = time.time()
         #
-        # prop_length = len(atomic_properties) + len(if_then_properties)
+        # prop_length = len(self.atomic_properties) + len(self.if_then_properties)
         # print("\n\nTime to verify " + str(prop_length) + " properties: " + str(ts1 - ts0))
 
 
@@ -312,6 +344,14 @@ if __name__ == "__main__":
     parser.add_argument('--skip_pickle', dest = 'do_pickle', action = 'store_false',
                         help = 'Option to skip the use of pickling')
     parser.set_defaults(do_pickle = True)
+
+    parser.add_argument('--compression', type = int, default = 6,
+                        help = 'Level of compression to use with pickling. Range: 0 (no compression) to 9 (high compression) (default: 6)')
+    parser.set_defaults(compression = 6)
+
+    parser.add_argument('--slice', type = int, default = 0,
+                        help = 'Index of contract to slice for. Range: 0 (no slicing) to #CONTRACTS (default: 0)')
+    parser.set_defaults(slice = 0)
 
     parser.add_argument('--no_svg', dest = 'draw_svg', action = 'store_false',
                         help = 'Flag to force svg files to not be drawn')
