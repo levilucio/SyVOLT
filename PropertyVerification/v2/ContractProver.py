0ef739e3fb194513c6d96bdc5d6a65eed0225c0e
Bentley James Oakes <bentleyjoakes@gmail.com>
2015-09-16 11:32:43 -0400

    WIP New property prover.

--- PropertyVerification/v2/ContractProver.py
+++ PropertyVerification/v2/ContractProver.py
@@ -41,70 +41,88 @@ class ContractProver():
 
         for pc in pathCondGen.get_path_conditions():
 
-            print("\nPC: " + pc.name)
+
+
+            if pc.name == "HEmptyPathCondition":
+                continue
 
             disambig_dict = {}
             for contract_name, atomic_contract in atomic_contracts:
 
 
-                #print("Checking contract: " + contract_name)
+
                 result = atomic_contract.check_isolated(pc)
 
                 # the isolated part did not match
                 # skip checking this contract on this pc
                 if result == atomic_contract.NO_ISOLATED:
+                    print("NO ISOLATED")
                     continue
+                # else:
+                #     print("ISOLATED")
+
+                print("\nPC: " + pc.name)
+                print("Checking contract: " + contract_name)
 
 
+                result = atomic_contract.check(pc)
+                if result == atomic_contract.NO_COMPLETE:
+                    print("Atomic contract: " + contract_name + " does not hold on " + pc.name + "\n")
+                    if pc not in contract_failed_pcs[contract_name]:
+                        contract_failed_pcs[contract_name].append(pc)
+                else:
+                    print("Atomic contract: " + contract_name + " does hold on " + pc.name + "\n")
+                    #contract_holds = True
+                    #break
 
-                rules = contract_rule_dict[contract_name]
+                #rules = contract_rule_dict[contract_name]
                 # print("Is in contract_rule_dict: ")
                 # for rule in rules:
                 #     print(rule)
 
-                result = self.seen_pc_before(pc.name, rules)
+                # result = self.seen_pc_before(pc.name, rules)
+                #
+                # if result:
+                #     #print("Contract holds because rule set has been seen before")
+                #     continue
 
-                if result:
-                    #print("Contract holds because rule set has been seen before")
-                    continue
-
-                #get the list of mms that will be disambigged on
-                contract_mms = self.disambig.set_contract(atomic_contract)
-
-                #check to see if we've already disambigged these for these mms
-                if str(contract_mms) in disambig_dict:
-                    disambig_pcs = disambig_dict[str(contract_mms)]
-                else:
-                    #do the disambigging
-                    disambig_pcs = self.disambig.disambiguate(pc)
-                    #disambig_pcs.append(pc)
-                    disambig_dict[str(contract_mms)] = disambig_pcs
-
-
-
-                #print("Length disambig pcs: " + str(len(dpcs)))
-
-                contract_holds = False
-
-                for dpc in disambig_pcs:
-                    #print("DPC: " + dpc.name)
-                    result = atomic_contract.check(dpc)
-
-                    #print("Result: " + str(result))
-
-                    if result == atomic_contract.NO_COMPLETE:
-                        print("Atomic contract: " + contract_name + " does not hold on " + pc.name + "\n")
-                        if pc not in contract_failed_pcs[contract_name]:
-                            contract_failed_pcs[contract_name].append(pc)
-                    else:
-                        #print("Atomic contract: " + contract_name + " does hold on " + pc.name + "\n")
-                        contract_holds = True
-                        break
-
-                if contract_holds:
-                    rules = self.rule_name_set(pc.name)
-                    if rules not in contract_rule_dict[contract_name]:
-                        contract_rule_dict[contract_name].append(rules)
+                # #get the list of mms that will be disambigged on
+                # contract_mms = self.disambig.set_contract(atomic_contract)
+                #
+                # #check to see if we've already disambigged these for these mms
+                # if str(contract_mms) in disambig_dict:
+                #     disambig_pcs = disambig_dict[str(contract_mms)]
+                # else:
+                #     #do the disambigging
+                #     disambig_pcs = self.disambig.disambiguate(pc)
+                #     #disambig_pcs.append(pc)
+                #     disambig_dict[str(contract_mms)] = disambig_pcs
+                #
+                #
+                #
+                # #print("Length disambig pcs: " + str(len(dpcs)))
+                #
+                # contract_holds = False
+                #
+                # for dpc in disambig_pcs:
+                #     #print("DPC: " + dpc.name)
+                #     result = atomic_contract.check(dpc)
+                #
+                #     #print("Result: " + str(result))
+                #
+                #     if result == atomic_contract.NO_COMPLETE:
+                #         print("Atomic contract: " + contract_name + " does not hold on " + pc.name + "\n")
+                #         if pc not in contract_failed_pcs[contract_name]:
+                #             contract_failed_pcs[contract_name].append(pc)
+                #     else:
+                #         #print("Atomic contract: " + contract_name + " does hold on " + pc.name + "\n")
+                #         contract_holds = True
+                #         break
+                #
+                # if contract_holds:
+                #     rules = self.rule_name_set(pc.name)
+                #     if rules not in contract_rule_dict[contract_name]:
+                #         contract_rule_dict[contract_name].append(rules)
 
         print("")
         for contract_name, atomic_contract in atomic_contracts:
--- PropertyVerification/v2/atomic_contract.py
+++ PropertyVerification/v2/atomic_contract.py
@@ -8,6 +8,7 @@ from t_core.messages import Packet
 from t_core.matcher import Matcher
 
 from core.himesis_utils import graph_to_dot
+from core.himesis_plus import look_for_attached
 
 class AtomicContract():
     '''
@@ -40,36 +41,80 @@ class AtomicContract():
         # detail the status of the property
         self.status = self.NOT_CHECKED
 
+        self.contract_mms = [mm.replace("MT_pre__", "") for mm in self.complete.vs["mm__"]]
+
+        #graph_to_dot(self.complete.name, self.complete)
+
+
+        self.connected_data = self.get_data(self.connected)
+        self.complete_data = self.get_data(self.complete)
+
+        #print("Connected Data: " + str(self.connected_data))
+        #print("Complete Data: " + str(self.complete_data))
+
 
     def check_isolated(self, pc):
-        p = Packet()
-        p.graph = pc
-        self.isolated_matcher.packet_in(p)
 
-        # the isolated was not found
-        if self.isolated_matcher.is_success:
-            return self.ISOLATED
-        else:
-            return self.NO_ISOLATED
+        pc_mms = pc.vs["mm__"]
+
+        #print("Contract MMs: " + str(self.contract_mms))
+        #print("PC MMs: " + str(pc_mms))
+
+        for mm in self.contract_mms:
+            if mm not in pc_mms:
+                #print("Fail on " + mm)
+                return self.NO_ISOLATED
+
+        return self.ISOLATED
 
 
     def check(self, pc):
-        p = Packet()
-        p.graph = pc
-        self.connected_matcher.packet_in(p)
 
-        # the connected was not found
-        # return success
-        if not self.connected_matcher.is_success:
+        self.pc_data = self.get_data(pc)
+
+        if self.match(self.connected, self.connected_data, pc):
+            print("Connected")
+        else:
             return self.NO_CONNECTED
 
-        p = Packet()
-        p.graph = pc
-        self.complete_matcher.packet_in(p)
-
-        # the complete was not found
-        # return success
-        if not self.complete_matcher.is_success:
-            return self.NO_COMPLETE
+        if self.match(self.complete, self.complete_data, pc):
+            print("Complete")
+            return self.COMPLETE_FOUND
         else:
-            print("COMPLETE MATCHED")
+            return self.NO_COMPLETE
+
+
+    def get_data(self, graph):
+        directLinks = []
+        mms = [mm.replace("MT_pre__", "") for mm in graph.vs["mm__"]]
+
+        for i in range(len(graph.vs)):
+            node = graph.vs[i]
+
+            mm = node["mm__"]
+
+            if "directLink" in mm:
+                neighbours = look_for_attached(i, graph)
+
+                directLinks.append(neighbours)
+
+        #print("Contract directLinks: " + graph.name + str(directLinks))
+
+        return [directLinks]
+
+    def match(self, contract, contract_data, pc):
+
+        print("Contract data: " + str(contract_data))
+        print("PC data: " + str(self.pc_data))
+
+        d_links = contract_data[0]
+        for n0, n1, link in d_links:
+
+            # print(contract.vs[n0]["mm__"])
+            # print(contract.vs[n1]["mm__"])
+            # print(contract.vs[link]["mm__"])
+
+            if [n0, n1, link] not in self.pc_data:
+                return False
+
+        return True
