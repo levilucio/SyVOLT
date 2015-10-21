d8b9c2631ca6436f9094b685192cefc41ee59fa6
Bentley James Oakes <bentleyjoakes@gmail.com>
2015-10-21 16:42:20 -0400

    Slicer now uses decomposed graphs.

---
+++ util/decompose_graph.py
@@ -0,0 +1,285 @@
+
+from core.himesis_plus import look_for_attached
+
+
+def decompose_graph(graph, verbosity = 0):
+    #decompose graph into directLinks, backwardLinks, and isolated elements
+
+    debug = False
+
+    if debug:
+        print("\nDecomposing graph: " + graph.name)
+
+    direct_links = []
+    backward_links = []
+    match_elements = []
+    isolated_match_elements = []
+    apply_elements = []
+
+
+    dls = []
+    bls = []
+
+
+    vs = graph.vs
+    mms = vs["mm__"]
+    for i in range(len(vs)):
+        v = vs[i]
+        mm = mms[i]
+
+        #print("MM: " + mm)
+        if "directLink" in mm:
+            dls.append(i)
+        elif "trace_link" in mm or "backward_link" in mm:
+            bls.append(i)
+        else:
+            if mm in ["MatchModel", "ApplyModel", "paired_with"]:
+                continue
+
+            neighbours = look_for_attached(i, graph)
+            for n in neighbours[:1]:
+                n_mm = mms[n]
+                if n_mm == "match_contains":
+                    match_elements.append(v)
+                    break
+                elif n_mm == "apply_contains":
+                    apply_elements.append(v)
+                    break
+
+
+    for dl in dls:
+
+        neighbours = look_for_attached(dl, graph)
+
+        n_link = neighbours[0]
+        n0_neighbours = look_for_attached(n_link, graph)
+        n0_neighbours_mm = [vs[n]["mm__"] for n in n0_neighbours]
+
+        #we don't care about direct links in the apply part
+        if 'apply_contains' in n0_neighbours_mm:
+            continue
+
+        n0 = neighbours[0]
+        n1 = neighbours[1]
+        direct_links.append((n0, n1, dl))
+
+    if debug:
+        print("Direct links: " + str(direct_links))
+
+    for bl in bls:
+
+        neighbours = look_for_attached(bl, graph)
+        n0 = neighbours[0]
+        n1 = neighbours[1]
+        backward_links.append((n0, n1, None))
+
+    if debug:
+        print("Backward links: " + str(backward_links))
+
+
+    #see if there are any match elements which are not in a direct link
+    for mm in match_elements:
+        #print("Match element: " + mm)
+        found = False
+        for (n1, n2, n_link) in direct_links:
+            mm1 = mms[n1]
+            mm2 = mms[n2]
+            #print("MM2: " + mm2)
+            if mm == mm1 or mm == mm2:
+                found = True
+
+        if not found:
+            isolated_match_elements.append(mm)
+
+
+    for me in match_elements:
+        me = me.index
+
+        for ae in apply_elements:
+
+            ae = ae.index
+
+            found_link = False
+            for a, b, c in backward_links:
+                if (a == me and b == ae) or (b == me and a == ae):
+                    found_link = True
+
+            if not found_link:
+                backward_links.append((me, ae, None))
+
+
+    # if debug:
+    #     print("\nRule name: " + graph.name)
+    #     print("Match contains: " + str(match_elements))
+    #     print("Isolated match elements: " + str(isolated_match_elements))
+    #     print("Apply contains: " + str(apply_elements))
+
+    return direct_links, backward_links, match_elements, isolated_match_elements, apply_elements
+
+
+def match_links(matcher, pattern, pattern_dls, graph, graph_dls, verbosity=0):
+
+
+    # print("\n=====================\nMatching links")
+    # print("Pattern: " + pattern.name + " Graph: " + graph.name)
+    #
+    # for patt0_n, patt1_n, patt_link_n in pattern_dls:
+    #
+    #     print("\nPattern nodes:")
+    #     if patt_link_n:
+    #         link = pattern.vs[patt_link_n]["mm__"]
+    #     else:
+    #         link = "backward_link"
+    #
+    #     print(pattern.vs[patt0_n]["mm__"] + " - " + link + " - " + pattern.vs[patt1_n]["mm__"])
+    #
+    # for graph_n0_n, graph_n1_n, graph_link_n in graph_dls:
+    #
+    #
+    #     print("\nGraph " + graph.name + " nodes:")
+    #     if graph_link_n:
+    #         link = graph.vs[graph_link_n]["mm__"]
+    #     else:
+    #         link = "backward_link"
+    #     print(graph.vs[graph_n0_n]["mm__"] + " - " + link + " - " + graph.vs[graph_n1_n]["mm__"])
+
+
+    for patt0_n, patt1_n, patt_link_n in pattern_dls:
+
+        # print("\nChecking Pattern " + pattern.name + " nodes:")
+        # if patt_link_n:
+        #     link = pattern.vs[patt_link_n]["mm__"]
+        # else:
+        #     link = "backward_link"
+        #
+        # print(pattern.vs[patt0_n]["mm__"] + " - " + link + " - " + pattern.vs[patt1_n]["mm__"])
+
+
+        found_match = False
+
+        for graph_n0_n, graph_n1_n, graph_link_n in graph_dls:
+
+
+            # print("\nChecking Graph " + graph.name + " nodes:")
+            # if graph_link_n:
+            #     link = graph.vs[graph_link_n]["mm__"]
+            # else:
+            #     link = "backward_link"
+            # print(graph.vs[graph_n0_n]["mm__"] + " - " + link + " - " + graph.vs[graph_n1_n]["mm__"])
+
+
+            nodes_match_1 = match_nodes(matcher, graph, graph_n0_n, pattern, patt0_n)
+            nodes_match_2 = match_nodes(matcher, graph, graph_n1_n, pattern, patt1_n)
+
+            nodes_match_3 = match_nodes(matcher, graph, graph_n1_n, pattern, patt0_n)
+            nodes_match_4 = match_nodes(matcher, graph, graph_n0_n, pattern, patt1_n)
+
+
+            nodes_match = (nodes_match_1 and nodes_match_2) or (nodes_match_3 and nodes_match_4)
+
+            #if nodes_match:
+            #    print("\nNodes found!")
+            # if not nodes_match:
+            #     print("Failure matching on " + pc.vs[pc_n0_n]["mm__"] + " vs " + contract.vs[n0_n]["mm__"])
+
+            # if not nodes_match:
+            #     print("Failure matching on " + pc.vs[pc_n1_n]["mm__"] + " vs " + contract.vs[n1_n]["mm__"])
+
+            if patt_link_n or graph_link_n:
+
+                nodes_match = nodes_match and match_nodes(matcher, graph, graph_link_n, pattern, patt_link_n)
+
+            if nodes_match:
+                found_match = True
+                #print("\nFound a link!")
+
+                if patt_link_n:
+                    link = pattern.vs[patt_link_n]["mm__"]
+
+                else:
+                    link = "backward_link"
+
+                #print("Pattern link: " + pattern.vs[patt0_n]["mm__"] + " - " + link + " - " + pattern.vs[patt1_n]["mm__"])
+
+                if graph_link_n:
+                    link = graph.vs[graph_link_n]["mm__"]
+
+                else:
+                    link = "backward_link"
+                #print("Graph link: " + graph.vs[graph_n0_n]["mm__"] + " - " + link + " - " + graph.vs[graph_n1_n]["mm__"])
+
+
+                return True
+                #pc_direct_links.remove([pc_n0_n, pc_n1_n, pc_link_n])
+                #break
+            # else:
+            #     if verbosity > 1:
+            #         print("\nNot match on:")
+            #         print(graph.vs[graph_n0_n]["mm__"])
+            #         print(graph.vs[graph_n1_n]["mm__"])
+            #
+            #         if graph_link_n:
+            #             print(graph.vs[graph_link_n]["mm__"])
+            #         print()
+
+        # if not found_match:
+        #     if verbosity > 1:
+        #         print("No direct link matches found")
+        #         print("Couldn't find:")
+        #         print(pattern.vs[patt0_n]["mm__"])
+        #         print(pattern.vs[patt1_n]["mm__"])
+        #         if patt_link_n:
+        #             print(pattern.vs[patt_link_n]["mm__"])
+        #         print()
+        #     return False
+    return False
+
+
+def match_nodes(matcher, graph, graph_node, pattern, patt_node):
+    sourceMM = graph.vs[graph_node]["mm__"].replace("MT_pre__", "")
+    targetMM = pattern.vs[patt_node]["mm__"].replace("MT_pre__", "")
+
+
+
+
+    if sourceMM != targetMM:
+
+        superclasses_dict = pattern["superclasses_dict"]
+        #print("Superclasses: " + str(superclasses_dict))
+
+
+
+        #print("Superclasses: " + str(superclasses_dict))
+
+        if not superclasses_dict[sourceMM] or targetMM not in superclasses_dict[sourceMM]:
+            return False
+
+    # print("Source MM: " + sourceMM)
+    # print("Target MM: " + targetMM)
+
+    graph_attrib = graph.vs[graph_node]["attr1"]
+
+    try:
+        pattern_attrib = pattern.vs[patt_node]["attr1"]
+    except KeyError:
+        pattern_attrib = pattern.vs[patt_node]["MT_pre__attr1"]
+
+
+    #return graph.vs[graph_node]["attr1"]
+
+    if graph_attrib == pattern_attrib:
+        #print(graph_attrib)
+        #print(pattern_attrib)
+        return True
+
+    if "return True" in pattern_attrib:
+        return True
+
+    if graph_attrib in pattern_attrib:
+        #print(graph_attrib)
+        #print(pattern_attrib)
+        return True
+
+    #are_feasible = matcher.are_semantically_feasible(graph_node, patt_node)
+    #print("Are feasible: " + str(are_feasible))
+    return False
--- util/slicer.py
+++ util/slicer.py
@@ -1,8 +1,13 @@
 
 from core.himesis_utils import graph_to_dot
+from util.decompose_graph import decompose_graph, match_links
 
-from util.rule_preprocessor import RulePreprocessor
+from PyRamify import PyRamify
 
+from core.match_algo import HimesisMatcher
+from core.himesis_plus import makePreConditionPattern
+
+from copy import deepcopy
 class Slicer():
 
 
@@ -10,24 +15,223 @@ class Slicer():
 
         self.debug = False
 
+        self.direct_links = {}
+        self.backward_links = {}
+        self.match_elements = {}
+        self.isolated_match_elements = {}
+        self.apply_elements = {}
 
 
         self.rules = rules
         self.transformation = transformation
 
 
-        self.rule_preprocessor = RulePreprocessor()
+        for layer in transformation:
+            for rule in layer:
 
-        self.rule_preprocessor.process_rules(self.rules, self.transformation)
+                rule_name = rule.name
+                dls, bls, mes, imes, aes = decompose_graph(self.rules[rule_name], verbosity=0)
+                self.direct_links[rule_name] = dls
+                self.backward_links[rule_name] = bls
+                self.match_elements[rule_name] = mes
+                self.isolated_match_elements[rule_name] = imes
+                self.apply_elements[rule_name] = aes
+
+
+                #if self.debug:
+                    #print("Drawing: " + rule.name)
+                    #graph_to_dot(rule.name, rule)
+
+
+
+        if self.debug:
+
+            print("Supertype hierarchy: ")
+            supertypes = list(self.rules.values())[0]["superclasses_dict"]
+            defaults = ['MetaModelElement_S', 'MetaModelElement_T']
+            for k in sorted(supertypes.keys()):
+                v = [s for s in supertypes[k] if s not in defaults]
+                if v:
+                    print(k + " : " + str(v))
+
+
+
+        for layer in transformation:
+            for rule in layer:
+
+                r_rules = self.find_required_rules(rule)
+                r_rules = sorted([r.name for r in r_rules])
+                print("Rule " + rule.name + " depends on: " + str(r_rules))
+
+
+
+
+    def find_required_rules(self, graph, is_contract = False, verbosity = 0):
+
+
+        if self.debug:
+            print("\nLooking for required rules for graph: " + graph.name)
+
+
+        try:
+            supertypes = graph["superclasses_dict"]
+        except KeyError:
+            print("Graph: " + graph.name + " does not have a superclasses dict")
+            supertypes = []
+
+
+
+
+        required_rules = []
+
+        for layer in self.transformation:
+
+            #don't look at the same layer that a rule is on
+            rule_names = [r.name for r in layer]
+
+            if graph.name in rule_names:
+                break
+
+            for rule in layer:
+
+                if rule in required_rules:
+                    continue
+
+                # rule_dls = self.direct_links[rule.name]
+                #
+                # if verbosity > 1:
+                #     print("\n" + rule.name)
+                #     print("Rule DLs: " + str(rule_dls))
+                #     print("Match Elements: " + str(self.match_elements[rule.name]))
+                #     print("Apply Elements: " + str(self.apply_elements[rule.name]))
+
+                #HACK: temp removal of equations
+                eqs = graph["equations"]
+                graph["equations"] = []
+
+                matcher = HimesisMatcher(rule, graph)
+                graph["equations"] = eqs
+
+                graph["superclasses_dict"] = rule["superclasses_dict"]
+
+                #matcher = None
+
+                if match_links(matcher, graph, self.direct_links[graph.name], rule, self.direct_links[rule.name], 2):
+                    required_rules.append(rule)
+
+                if match_links(matcher, graph, self.backward_links[graph.name], rule, self.backward_links[rule.name], 0):
+                    required_rules.append(rule)
+
+                # for dl in dls:
+                #
+                #     if dl in rule_dls:
+                #         required_rules.append(rule)
+                #         if self.debug:
+                #             print("Add rule (dl): " + rule.name)
+                #         break
+                #
+                #     else:
+                #
+                #         #check for supertype matching
+                #
+                #         a, b = dl
+                #         for rule_dl in rule_dls:
+                #             ra, rb = rule_dl
+                #
+                #             if a in supertypes and ra in supertypes[a] and \
+                #                 b in supertypes and rb in supertypes[b]:
+                #                 required_rules.append(rule)
+                #
+                #                 if self.debug:
+                #                     print("Add rule (dl-supertypes): " + rule.name)
+                #                 break
+
+
+                # for backward_link in bls:
+                #     a, m = backward_link
+                #
+                #     if self.debug:
+                #         print("M: " + m + " A: " + a)
+                #         print("Match Elements: " + str(self.match_elements[rule.name]))
+                #         print("Apply Elements: " + str(self.apply_elements[rule.name]))
+                #
+                #     if m in self.match_elements[rule.name] and \
+                #         a in self.apply_elements[rule.name]:
+                #         required_rules.append(rule)
+                #         found_bls.append(backward_link)
+                #         if self.debug:
+                #             print("Add rule (bl): " + rule.name)
+                #     else:
+                #
+                #         #check for supertype matching
+                #
+                #         if a in supertypes and supertypes[a] in self.apply_elements[rule.name] and \
+                #             m in supertypes and supertypes[m] in self.match_elements[rule.name]:
+                #             required_rules.append(rule)
+                #             found_bls.append(backward_link)
+                #             if self.debug:
+                #                 print("Add rule (bl-supertypes): " + rule.name)
+                #
+                #
+                # isolated_elements = self.isolated_match_elements[graph.name]
+                #
+                # if isolated_elements:
+                #
+                #     isolated_added = False
+                #     rule_mms = rule.vs["mm__"]
+                #     #print("Rule mms: " + str(rule_mms))
+                #     #print("Isolated mms: " + str(isolated_elements))
+                #
+                #     for isolated_mm in isolated_elements:
+                #         if isolated_mm in rule_mms:
+                #             required_rules.append(rule)
+                #             isolated_added = True
+                #             break
+
+
+
+                # print("WARNING: HAVENT ADDED RULE YET")
+                # print("Rule: " + rule.name)
+
+                #rule_bls = self.backward_links[rule.name]
+
+                # print("Rule BLs: " + str(rule_bls))
+                #
+                # print("Match Elements: " + str(self.match_elements[rule.name]))
+                # print("Apply Elements: " + str(self.apply_elements[rule.name]))
+                #
+                # print("Contract BLs: " + str(bls))
+
+                #print("\nRule " + rule.name + " MMS: " + str(rule_mms))
+
+                # for mm in mms_required:
+                #     if mm in rule_mms:
+                #         required_rules.append(rule)
+                #
+                #         #print("Add rule: " + rule.name + " with mms: " + str(rule_mms))
+                #         break
+        # if len(found_bls) < len(bls):
+        #     print("\nERROR: Could not find all backward links for graph: " + graph.name)
+        #     print("Missing backward links: " + str(["M: " + m + " -> A: " + a for a,m in bls if (a,m) not in found_bls]))
+
+        return list(set(required_rules))
 
 
     def slice_transformation(self, contract):
 
         contract = contract[1].complete
+        contract_name = contract.name
 
-        print("Slicing for: " + contract.name)
+        print("Slicing for: " + contract_name)
 
-        self.rule_preprocessor.decompose_graph(contract, verbosity=0)
+
+        dls, bls, mes, imes, aes = decompose_graph(contract, verbosity=0)
+        self.direct_links[contract_name] = dls
+        self.backward_links[contract_name] = bls
+        self.match_elements[contract_name] = mes
+        self.isolated_match_elements[contract_name] = imes
+        self.apply_elements[contract_name] = aes
+
 
         if self.debug:
             #print("Direct links: " + str(self.direct_links))
@@ -38,23 +242,27 @@ class Slicer():
                     graph_to_dot(rule.name, rule)
 
 
-        required_rules = self.rule_preprocessor.find_required_rules(contract, True, verbosity=0)
+        required_rules = self.find_required_rules(contract, True, verbosity=2)
 
 
         rr_names = [rule.name for rule in required_rules]
 
-        if self.debug:
-            print("Required rules for contract: " + str(rr_names))
+        print("Required rules for contract " + contract_name + ": " + str(sorted(rr_names)))
+
+
+
+
+        #raise Exception("Contract Required Rules")
 
         for rr in required_rules:
 
-            new_rrs = self.rule_preprocessor.find_required_rules(rr, self.transformation)
+            new_rrs = self.find_required_rules(rr, self.transformation)
             for new_rr in new_rrs:
                 if new_rr not in required_rules:
                     required_rules.append(new_rr)
 
         rr_names = [rule.name for rule in required_rules]
-        print("Required rules: " + str(sorted(set(rr_names))))
+        print("Required rules: " + str(sorted(rr_names)))
 
 
         new_rules = {}
