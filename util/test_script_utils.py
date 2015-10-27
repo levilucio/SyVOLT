8bed59a8ed9167b1f471bca55e707f0296e25bbf
Bentley James Oakes <bentleyjoakes@gmail.com>
2015-10-27 17:22:29 -0400

    Add get_sub_and_super to test script utils.

--- util/test_script_utils.py
+++ util/test_script_utils.py
@@ -1,4 +1,6 @@
 from core.himesis_utils import graph_to_dot
+from util.ecore_utils import EcoreUtils
+from core.himesis_plus import buildPreListFromClassNames
 
 def select_rules(full_transformation, num_rules):
     selected_transformation = []
@@ -17,3 +19,57 @@ def select_rules(full_transformation, num_rules):
 
 
 
+
+def get_sub_and_super_classes(inputMM, outputMM):
+    subclasses_dict = {}
+
+
+
+    inMM = EcoreUtils(inputMM)
+    subclasses_dict["MT_pre__MetaModelElement_S"] = buildPreListFromClassNames(inMM.getMetamodelClassNames())
+
+    inMMinheritanceTree = inMM.getSubClassInheritanceRelationForClasses()
+
+    for className in inMMinheritanceTree:
+        subclasses_dict["MT_pre__" + className] = buildPreListFromClassNames(inMMinheritanceTree[className])
+
+    outMM = EcoreUtils(outputMM)
+    subclasses_dict["MT_pre__MetaModelElement_T"] = buildPreListFromClassNames(outMM.getMetamodelClassNames())
+
+    outMMinheritanceTree = outMM.getSubClassInheritanceRelationForClasses()
+    for className in outMMinheritanceTree:
+        subclasses_dict["MT_pre__" + className] = buildPreListFromClassNames(outMMinheritanceTree[className])
+
+
+    superclasses_dict = {}
+
+    inMMinheritanceTree = inMM.getSuperClassInheritanceRelationForClasses()
+    for super in inMMinheritanceTree:
+        inMMinheritanceTree[super].append("MetaModelElement_S")
+
+    outMMinheritanceTree = outMM.getSuperClassInheritanceRelationForClasses()
+    for super in outMMinheritanceTree:
+        outMMinheritanceTree[super].append("MetaModelElement_T")
+
+    superclasses_dict.update(inMMinheritanceTree)
+    superclasses_dict.update(outMMinheritanceTree)
+
+    #remove duplicates
+    for superclass in superclasses_dict.keys():
+        superclasses_dict[superclass] = list(set(superclasses_dict[superclass]))
+
+
+    #
+    # print("\n\n")
+    # print("Subclasses dict:")
+    # for subclass in sorted(list(subclasses_dict.keys())):
+    #     sub_list = [sub.replace("MT_pre__", "") for sub in subclasses_dict[subclass]]
+    #     print(subclass.replace("MT_pre__", "") + " : " + str(sub_list))
+    # print("\n")
+    # print("Superclasses dict:")
+    # for superclass in sorted(list(superclasses_dict.keys())):
+    #     print(superclass + " : " + str(sorted(superclasses_dict[superclass])))
+    # print("\n\n")
+
+
+    return subclasses_dict, superclasses_dict
