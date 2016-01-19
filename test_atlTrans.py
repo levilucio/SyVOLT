bfe1a8c35bebb5a09bfc46637df0c8d4565a6fa5
Bentley James Oakes <bentleyjoakes@gmail.com>
2016-01-19 00:49:54 -0500

    Correctly prove propositional contract on original F2P test.

--- test_atlTrans.py
+++ test_atlTrans.py
@@ -9,69 +9,19 @@ import time
 import sys
 
 from path_condition_generator import PathConditionGenerator
+from PropertyVerification.v2.ContractProver import ContractProver
 
 from t_core.matcher import Matcher
 from t_core.messages import Packet
 
 from PyRamify import PyRamify
 
-from util.test_script_utils import select_rules, get_sub_and_super_classes
+from util.test_script_utils import select_rules, get_sub_and_super_classes, load_contracts
 from util.slicer import Slicer
 
-from core.himesis_utils import graph_to_dot
+from core.himesis_utils import graph_to_dot, load_directory
 # all runs are the same transformation, but with different metamodel elements
 # the purpose is to do scalability testing with multiple configurations and multiple sets of rules
-
-
-from PropertyVerification.v2.atomic_contract import AtomicContract
-from PropertyVerification.v2.ContractProver import ContractProver
-
-from PropertyVerification.v2.if_then_contract import IfThenContract
-from PropertyVerification.v2.prop_logic import NotContract, AndContract
-
-# from PropertyVerification.state_property import StateProperty
-# from PropertyVerification.atomic_state_property import AtomicStateProperty
-# from PropertyVerification.and_state_property import AndStateProperty
-# from PropertyVerification.or_state_property import OrStateProperty
-# from PropertyVerification.not_state_property import NotStateProperty
-# from PropertyVerification.implication_state_property import ImplicationStateProperty
-# from PropertyVerification.Not import Not #StateSpace Prop
-# from PropertyVerification.Implication import Implication #StateSpace Prop
-# from PropertyVerification.And import And #StateSpace Prop
-# from PropertyVerification.Or import Or #StateSpace Prop
-# from PropertyVerification.BACKUP_atomic_state_property import BKUPAtomicStateProperty
-# from PropertyVerification.PropertyVerifier import PropertyVerifier
-#from lib2to3.fixer_util import p1
-
-
-
-#positive
-
-#from PropertyVerification.HEmpty_IsolatedConnectedLHS import HEmpty_IsolatedConnectedLHS
-
-from ATLTrans.props.HfourMembers_IsolatedLHS import HfourMembers_IsolatedLHS
-from ATLTrans.props.HfourMembers_ConnectedLHS import HfourMembers_ConnectedLHS
-from ATLTrans.props.HfourMembers_CompleteLHS import HfourMembers_CompleteLHS
-
-from ATLTrans.props.HmotherFather_IsolatedLHS import HmotherFather_IsolatedLHS
-from ATLTrans.props.HmotherFather_ConnectedLHS import HmotherFather_ConnectedLHS
-from ATLTrans.props.HmotherFather_CompleteLHS import HmotherFather_CompleteLHS
-
-
-#negative
-from ATLTrans.props.HdaughterMother_IsolatedLHS import HdaughterMother_IsolatedLHS
-from ATLTrans.props.HdaughterMother_ConnectedLHS import HdaughterMother_ConnectedLHS
-from ATLTrans.props.HdaughterMother_CompleteLHS import HdaughterMother_CompleteLHS
-
-
-#implication
-from FamiliesToPersons_MM.Properties.Positive.Himesis.HCommunityPerson1_IsolatedLHS import HCommunityPerson1_IsolatedLHS
-from FamiliesToPersons_MM.Properties.Positive.Himesis.HCommunityPerson1_ConnectedLHS import HCommunityPerson1_ConnectedLHS
-from FamiliesToPersons_MM.Properties.Positive.Himesis.HCommunityPerson1_CompleteLHS import HCommunityPerson1_CompleteLHS
-
-from FamiliesToPersons_MM.Properties.Positive.Himesis.HCommunityPerson2_IsolatedLHS import HCommunityPerson2_IsolatedLHS
-from FamiliesToPersons_MM.Properties.Positive.Himesis.HCommunityPerson2_ConnectedLHS import HCommunityPerson2_ConnectedLHS
-from FamiliesToPersons_MM.Properties.Positive.Himesis.HCommunityPerson2_CompleteLHS import HCommunityPerson2_CompleteLHS
 
 
 
@@ -120,53 +70,30 @@ class Test():
             for rule in layer:
                 rule["superclasses_dict"] = superclasses_dict
 
-        FourMembers = [HfourMembers_IsolatedLHS(), HfourMembers_ConnectedLHS(), HfourMembers_CompleteLHS()]
-        for fm in FourMembers:
-            fm["superclasses_dict"] = superclasses_dict
- 
 
- 
- 
-        HMotherFather = [HmotherFather_IsolatedLHS(), HmotherFather_ConnectedLHS(), HmotherFather_CompleteLHS()]
-        for c in HMotherFather:
-            c["superclasses_dict"] = superclasses_dict
- 
+        # load the contracts
 
- 
- 
-        DaughterMother = [HdaughterMother_IsolatedLHS(), HdaughterMother_ConnectedLHS(), HdaughterMother_CompleteLHS()]
-        for c in DaughterMother:
-            c["superclasses_dict"] = superclasses_dict
+        contracts = load_directory("ATLTrans/props/")
 
+        atomic_contracts = [
+            #"daughterMother",
+            #"fourMembers",
+            #"motherFather",
+        ]
+        if_then_contracts = [
+            # ["S1_if", "S1_then"],
+        ]
+        prop_if_then_contracts = [
+            # structure is 'if graph', 'then graph'
+            # where the 'then graph' is made up of reverse polish notation
+            ["CommunityPerson_if", ["CommunityPerson_then", "NOT"]],
+        ]
 
-        HFourMembers_atomic = AtomicContract(FourMembers[0], FourMembers[1], FourMembers[2])
-        HMotherFather_atomic = AtomicContract(HMotherFather[0], HMotherFather[1], HMotherFather[2])
-        HDaughterMother_atomic = AtomicContract(DaughterMother[0], DaughterMother[1], DaughterMother[2])
+        self.atomic_contracts, self.if_then_contracts = load_contracts(contracts, superclasses_dict,
+                                                                       atomic_contracts, if_then_contracts,
+                                                                       prop_if_then_contracts,
+                                                                       args.draw_svg)
 
-
-        CommunityPerson1 = HCommunityPerson1_CompleteLHS()
-        CommunityPerson1["superclasses_dict"] = superclasses_dict
-
-        CommunityPerson2 = HCommunityPerson2_CompleteLHS()
-        CommunityPerson2["superclasses_dict"] = superclasses_dict
-
-        HCommunityPersonIfClause = AtomicContract(HCommunityPerson1_IsolatedLHS(), HCommunityPerson1_ConnectedLHS(), CommunityPerson1)
-
-        HCommunityPersonThenClause1 = AtomicContract(HCommunityPerson1_IsolatedLHS(), HCommunityPerson1_ConnectedLHS(),
-                                                  CommunityPerson1)
-
-        HCommunityPersonThenClause = AndContract(HCommunityPersonThenClause1,
-                                     NotContract(
-            AtomicContract(HCommunityPerson2_IsolatedLHS(), HCommunityPerson2_ConnectedLHS(), CommunityPerson2)))
- 
-        HCommunityPerson_IfThen = IfThenContract(HCommunityPersonIfClause, HCommunityPersonThenClause)
-
-        #atomic_properties = [["HDaughterMother_atomic", HDaughterMother_atomic]]
-        self.atomic_contracts = [["HDaughterMother_atomic", HDaughterMother_atomic], ["HFourMembers_atomic", HFourMembers_atomic],["HMotherFather_atomic", HMotherFather_atomic]]
-
-
-        #self.atomic_contracts = [self.atomic_contracts[0]]
-        self.if_then_contracts = [] #[["HCommunityPerson", HCommunityPerson_IfThen]]
 
         if args.slice > 0:
             contract = self.atomic_contracts[args.slice - 1]
@@ -175,7 +102,7 @@ class Test():
             slicer = Slicer(self.rules, self.transformation)
 
             print("Number rules before: " + str(len(self.rules)))
-            self.rules, self.transformation = slicer.slice_transformation(contract)
+            self.new_rules, self.new_transformation = slicer.slice_transformation(contract)
             print("Number rules after: " + str(len(self.rules)))
 
 
@@ -237,7 +164,7 @@ class Test():
         #print("printing path conditions")
         #s.print_path_conditions_screen()
 
-        raise Exception()
+        #raise Exception()
 
         #s.print_path_conditions_file()
 
