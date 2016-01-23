8bdf98f27151a5f3e21db2f35674a3d255273e5b
Bentley James Oakes <bentleyjoakes@gmail.com>
2016-01-23 18:17:16 -0500

    Add handbuilt GM transformation files (generated through Eclipse.

--- test_GM2Autosar_transformation.py
+++ test_GM2Autosar_transformation.py
@@ -26,31 +26,47 @@ class Prover():
 
     def do_proof(self,args):    
 
+        do_old_transformation = True
+
         pyramify = PyRamify(verbosity=args.verbosity, draw_svg=args.draw_svg)
         
+        if do_old_transformation:
+            r0 = 'HMapPN2FiveElements'
+            r1 = 'HMapPartition'
+            r2 = 'HMapModule'
+            r3 = 'HConnECU2VirtualDevice1'
+            r4 = 'HConnVirtualDevice2Distributable1'
+            r5 = 'HConnectPPortPrototype'
+            r6 = 'HConnectRPortPrototype'
+            r7 = 'HConnVirtualDevice2Distributable2'
+            r8 = 'HConnECU2VirtualDevice2'
 
-        r0 = 'HcreateComponent'
-        r1 = 'HinitSysTemp'
-        r2 = 'HinitSingleSwc2EcuMapping'
-        r3 = 'HsysmappingswMappingSolveRefPhysicalNodePartitionSystemMappingSwcToEcuMapping'
-        r4 = 'HcompostypecomponentSolveRefPhysicalNodePartitionModuleCompositionTypeComponentPrototype'
-        r5 = 'HcompostypeportSolveRefPhysicalNodePartitionModuleSchedulerServiceCompositionTypePPortPrototype'
-        r6 = 'HcompostypeportSolveRefPhysicalNodePartitionModuleSchedulerServiceCompositionTypeRPortPrototype'
-        r7 = 'HmappingcomponentSolveRefPartitionModuleSwcToEcuMappingSwCompToEcuMappingcomponent'
-        r8 = 'HmappingecuInstanceSolveRefPhysicalNodePartitionSwcToEcuMappingEcuInstance'
+            full_transformation = [[r0, r1, r2, ], [r3, ], [r4, ], [r7, ], [r8, ], [r5, r6, ], ]
 
+            rule_dir = "GM2AUTOSAR_MM/transformation"
 
-                             
-        full_transformation = [[r0,],[r1,r2,],[r3,],[r4,],[r5,],[r6,],[r7,],[r8,],]
+        else:
+            r0 = 'HcreateComponent'
+            r1 = 'HinitSysTemp'
+            r2 = 'HinitSingleSwc2EcuMapping'
+            r3 = 'HsysmappingswMappingSolveRefPhysicalNodePartitionSystemMappingSwcToEcuMapping'
+            r4 = 'HcompostypecomponentSolveRefPhysicalNodePartitionModuleCompositionTypeComponentPrototype'
+            r5 = 'HcompostypeportSolveRefPhysicalNodePartitionModuleSchedulerServiceCompositionTypePPortPrototype'
+            r6 = 'HcompostypeportSolveRefPhysicalNodePartitionModuleSchedulerServiceCompositionTypeRPortPrototype'
+            r7 = 'HmappingcomponentSolveRefPartitionModuleSwcToEcuMappingSwCompToEcuMappingcomponent'
+            r8 = 'HmappingecuInstanceSolveRefPhysicalNodePartitionSwcToEcuMappingEcuInstance'
+            full_transformation = [[r0,],[r1,r2,],[r3,],[r4,],[r5,],[r6,],[r7,],[r8,],]
+
+            rule_dir = "GM2AUTOSAR_MM/transformation_from_ATL/"
         
-        self.rules, self.transformation = pyramify.get_rules("GM2AUTOSAR_MM/transformation_from_ATL/", full_transformation)
+        self.rules, self.transformation = pyramify.get_rules(rule_dir, full_transformation)
         
         inputMM = "GM2AUTOSAR_MM/metamodels/Industrial.ecore"
         outputMM = "GM2AUTOSAR_MM/metamodels/Autosar.ecore"
         subclasses_dict, superclasses_dict = get_sub_and_super_classes(inputMM, outputMM)
 
         [self.rules, self.ruleTraceCheckers, backwardPatterns2Rules, backwardPatternsComplete, self.matchRulePatterns, self.ruleCombinators, self.overlapping_rules, self.subsumption, self.loopingRuleSubsumption] = \
-            pyramify.ramify_directory("GM2AUTOSAR_MM/transformation_from_ATL/", self.transformation)
+            pyramify.ramify_directory(rule_dir, self.transformation)
 
         for layer in self.transformation:
             for r in layer:
@@ -72,7 +88,7 @@ class Prover():
         contracts = load_directory("GM2AUTOSAR_MM/Properties/from_eclipse/")
 
         atomic_contracts = [
-            #"P1",
+            "P1",
             #"P2",
         ]
         if_then_contracts = [
@@ -83,10 +99,10 @@ class Prover():
         prop_if_then_contracts = [
             #structure is 'if graph', 'then graph'
             #where the 'then graph' is made up of reverse polish notation
-            ["M2_if", ["M2_then1", "M2_then2", "NOT", "AND"]],
-            ["M4_if", ["M4_then1", "M4_then2", "NOT", "AND"]],
-            ["M5_if", ["M5_then1", "M5_then2", "NOT", "AND"]],
-            ["M6_if", ["M6_then1", "M6_then2", "NOT", "AND"]],
+            # ["M2_if", ["M2_then1", "M2_then2", "NOT", "AND"]],
+            # ["M4_if", ["M4_then1", "M4_then2", "NOT", "AND"]],
+            # ["M5_if", ["M5_then1", "M5_then2", "NOT", "AND"]],
+            # ["M6_if", ["M6_then1", "M6_then2", "NOT", "AND"]],
 
         ]
 
