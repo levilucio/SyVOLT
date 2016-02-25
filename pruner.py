686ea3deefb45a5be372126707afb98bcdf6eed0
Bentley James Oakes <bentleyjoakes@gmail.com>
2016-02-25 15:27:14 -0500

    Start making unified function to resolve containment links.

--- pruner.py
+++ pruner.py
@@ -18,7 +18,7 @@ class Pruner(object):
         Constructor
         '''
 
-        self.debug = False
+        self.debug = True
 
         self.eu = EcoreUtils(metamodel)
         #self.mmContainmentLinks = self.eu.getContainmentLinksForClasses()
@@ -27,6 +27,8 @@ class Pruner(object):
         self.ruleMissingContLinks = {}
 
         self.linksToRules = {}
+
+        self.rule_names = rule_names
 
         self.mmClassParents = self.eu.getSuperClassInheritanceRelationForClasses()
         self.mmClassChildren = self.eu.getSubClassInheritanceRelationForClasses()
@@ -49,64 +51,65 @@ class Pruner(object):
 
                 required_rules[rule.name] = {}
 
-                #look at each class that the rule is missing
-                for className, link_names in self.ruleMissingContLinks[rule.name].items():
+                links_not_found = self.will_links_be_built(self.ruleMissingContLinks[rule.name], self.ruleContainmentLinks)
 
-                    #look at each link the class is missing
-                    for source_class_name, link_name in link_names:
+                if len(links_not_found) > 0:
 
-                        found_link = False
+                    link = links_not_found[0]
+                    className = link[0]
+                    source_class_name = link[1]
 
-                        # print("\nLooking for:")
-                        # print(className + " : " + source_class_name + " : " + link_name)
-                        #
-                        # print(self.ruleContainmentLinks)
+                    print("Error for rule: " + self.rule_names[rule.name])
+                    for l in links_not_found:
+                        print(l)
 
-                        link = (source_class_name, link_name)
+                    print("Containment links:")
+                    for k, v in sorted(self.ruleContainmentLinks.items()):
+                        print(k + " :")
+                        for c in v:
+                            print("\t" + str(c) + " : " + str(v[c]) )
 
-                        #look at the other rules
-                        for contain_rule_name, contain_links in self.ruleContainmentLinks.items():
+                    print("Supertypes:")
+                    try:
+                        print(self.mmClassParents[className])
+                    except KeyError:
+                        pass
 
-                            if not contain_links:
-                                continue
+                    print("Subtypes:")
+                    try:
+                        print(self.mmClassChildren[className])
+                    except KeyError:
+                        pass
 
-                            real_contain_rule_name = rule_names[contain_rule_name]
+                    print("Supertypes:")
+                    try:
+                        print(self.mmClassParents[source_class_name])
+                    except KeyError:
+                        pass
 
-                            #print(real_contain_rule_name + " : " + str(contain_links))
+                    print("Subtypes:")
+                    try:
+                        print(self.mmClassChildren[source_class_name])
+                    except KeyError:
+                        pass
 
-                            parentClasses = []
-                            if className in self.mmClassParents:
-                                parentClasses = self.mmClassParents[className]
+                    raise Exception()
 
-                            childClasses = []
-                            if className in self.mmClassChildren:
-                                childClasses = self.mmClassChildren[className]
+                # if found_link:
+                #     full_link_name = className + "/" + link_name
+                #
+                #     try:
+                #         if real_contain_rule_name not in required_rules[rule.name][full_link_name]:
+                #             required_rules[rule.name][full_link_name].append(real_contain_rule_name)
+                #     except KeyError:
+                #         required_rules[rule.name][full_link_name] = [real_contain_rule_name]
+                # else:
+                #     message = "\nError: No rule builds containment link '" + source_class_name + "' -> '" + link_name + "' -> '" + className + "' from rule.name " + rule_names[rule.name]
+                #     required_rules[rule.name][className + "/" + link_name] = ["None!"]
+                #
+                #     print(message)
+                #
 
-                            for cl_name in [className] + parentClasses + childClasses:
-                                if cl_name in contain_links.keys() and link in contain_links[cl_name]:
-                                    found_link = True
-                                    break
-
-                        if found_link:
-                            full_link_name = className + "/" + link_name
-
-                            try:
-                                if real_contain_rule_name not in required_rules[rule.name][full_link_name]:
-                                    required_rules[rule.name][full_link_name].append(real_contain_rule_name)
-                            except KeyError:
-                                required_rules[rule.name][full_link_name] = [real_contain_rule_name]
-                        else:
-                            message = "Error: No rule builds containment link '" + link_name + "' for class " + className + " from rule.name " + rule_names[rule.name]
-                            required_rules[rule.name][className + "/" + link_name] = ["None!"]
-
-                            print(message)
-
-                            print("Containment links:")
-                            for k, v in self.ruleContainmentLinks.items():
-                                print(k + " : " + str(v))
-
-                            if self.debug:
-                               raise Exception(message)
 
 
 
@@ -129,7 +132,84 @@ class Pruner(object):
             print("MM class parents:")
             for mm in sorted(self.mmClassParents):
                 print(mm + " : " + str(self.mmClassParents[mm]))
-        
+
+    def will_links_be_built(self, contain_links, future_contain_links):
+
+        links_not_found = []
+
+        # look at each class that the rule is missing
+        for className, link_names in contain_links.items():
+            parentClasses = []
+            if className in self.mmClassParents:
+                parentClasses = self.mmClassParents[className]
+
+            childClasses = []
+            if className in self.mmClassChildren:
+                childClasses = self.mmClassChildren[className]
+
+
+            # look at each link the class is missing
+            for source_class_name, link_name in link_names:
+                found_link = False
+
+                print("\nLooking for:")
+                print(className + " : " + source_class_name + " : " + link_name)
+                print(str([className] + parentClasses + childClasses))
+                #
+                # print(self.ruleContainmentLinks)
+
+                # look at the other rules
+                for contain_rule_name, contain_links in future_contain_links.items():
+                    if not contain_links:
+                        continue
+
+                    if found_link:
+                        break
+
+                    #real_contain_rule_name = self.rule_names[contain_rule_name]
+
+                    # print(real_contain_rule_name + " : " + str(contain_links))
+
+                    #try all possibilities for the className
+                    for cl_name in [className] + parentClasses + childClasses:
+                        if cl_name not in contain_links.keys():
+                            continue
+
+                        if found_link:
+                            break
+
+                        #look at all links
+                        for other_link in contain_links[cl_name]:
+
+                            print("\n\tComparing to:")
+                            print("\t" + cl_name + " : " + other_link[0] + " : " + other_link[1])
+
+
+                            #the link name doesn't match
+                            if link_name != other_link[1]:
+                                continue
+
+                            #now check to see if the source class is in the inheritance heirarchy
+                            source_class = [source_class_name]
+                            if source_class_name in self.mmClassParents:
+                                source_class += self.mmClassParents[source_class_name]
+                            if source_class_name in self.mmClassChildren:
+                                source_class += self.mmClassChildren[source_class_name]
+
+                            if other_link[0] not in source_class:
+                                print("Source class not in heirarchy")
+                                continue
+
+                            print("Found the link")
+                            found_link = True
+                            break
+
+                if not found_link:
+                    links_not_found.append((className, source_class_name, link_name))
+
+        return links_not_found
+
+
 
     def getContainmentLinksBuiltByRuleSet(self, ruleNames):
         '''
