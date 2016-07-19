67349b6bb00144377479a4e620ce0a081fed5e89
Bentley James Oakes <bentleyjoakes@gmail.com>
2016-07-19 19:23:46 +0200

    Restore do_cprofile.

--- profiler.py
+++ profiler.py
@@ -31,6 +31,64 @@ except ImportError:
 import pstats
 from random import randint
 
+import pstats
+def do_cprofile(func):
+    def profiled_func(*args, **kwargs):
+        profile = pp.Profile()
+        try:
+            profile.enable()
+            if global_profile_memory:
+                global_hp.setref()
+
+            result = func(*args, **kwargs)
+            profile.disable()
+            return result
+        finally:
+
+
+            print("\nFunction: " + str(func.__name__))
+
+            if global_profile_memory:
+                h = global_hp.heap()
+                print("\nMemory usage:")
+                print(h)
+
+                print("\nBy id:")
+                print(h.byid)
+                print(h.byvia)
+                print(h.byrcs)
+                print(h.referents)
+
+                print("\nreferents[0]:")
+                print(h.referents[0].byid)
+                print(h.referents[0].byvia)
+                print(h.referents[0].byrcs)
+                print(h.referents[0].referents)
+
+
+
+            s = StringIO()
+            sortby = 'time'
+            ps = pstats.Stats(profile, stream = s).sort_stats(sortby)
+            ps.print_stats()
+
+            print("Time usage:")
+            time_table = str(s.getvalue()).split("\n")
+            for i in range(25):
+                if i >= len(time_table):
+                    break
+
+                if time_table[i].strip() == "":
+                    continue
+
+                print(time_table[i])
+
+
+
+
+            print("")
+    return profiled_func
+
 class Profiler(object):
 
     profiler = None
