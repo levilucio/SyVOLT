3ac2ff267445113c0e37c2fd682f62f1190edca8
Bentley James Oakes <bentleyjoakes@gmail.com>
2016-08-05 16:13:57 +0200

    Make test_atlTrans script just contain the data needed.

--- test_atlTrans_extended.py
+++ test_atlTrans_extended.py
@@ -4,33 +4,18 @@ Created on 2013-01-22
 @author: levi
 '''
 
-import unittest
-import time
-import sys
-
-from path_condition_generator import PathConditionGenerator
-
-from pyramify.PyRamify import PyRamify
-
-from util.test_script_utils import select_rules, get_sub_and_super_classes,\
-    load_transformation, changePropertyProverMetamodel, set_supertypes, load_contracts, save_pcs, load_pcs
-from util.slicer import Slicer
-
-from core.himesis_utils import graph_to_dot, load_directory
-
-# all runs are the same transformation, but with different metamodel elements
-# the purpose is to do scalability testing with multiple configurations and multiple sets of rules
-
-
-from PropertyVerification.ContractProver import ContractProver
 
 from util.parser import load_parser
 
-class Test:
+from util.test_script_base import Test
 
-    def setUp(self, args):
+class ATLTest(Test):
 
-        full_transformation = [
+    def __init__(self):
+        super(ATLTest, self).__init__()
+
+        #============TRANSFORMATION=================
+        self.full_transformation = [
             ['HCountry2Community'],
             ['HFather2Man'],
             ['HMother2Woman'],
@@ -54,49 +39,21 @@ class Test:
             ['HdfacilitiesSolveRefNeighborhoodSchoolServiceChildDistrictSpecialFacilityPerson']
 
         ]
-        self.rules, self.transformation = load_transformation("ExFamToPerson/transformation/no_contains", full_transformation)
+
+        self.transformation_directory = "ExFamToPerson/transformation/no_contains"
 
 
+        #=====METAMODELS===============
 
-        inputMM = "ExFamToPerson/Families_Extended.ecore"
-        outputMM = "ExFamToPerson/Persons_Extended.ecore"
-
-        self.subclasses_dict, self.superclasses_dict = get_sub_and_super_classes(inputMM, outputMM)
-
-        pre_metamodel = ["MT_pre__FamiliesToPersons_MM", "MoTifRule"]
-        post_metamodel = ["MT_post__FamiliesToPersons_MM", "MoTifRule"]
-
-        changePropertyProverMetamodel(pre_metamodel, post_metamodel, self.subclasses_dict, self.superclasses_dict)
+        self.inputMM = "ExFamToPerson/Families_Extended.ecore"
+        self.outputMM = "ExFamToPerson/Persons_Extended.ecore"
 
 
+        #====CONTRACTS==================
 
+        self.contract_directory = "ExFamToPerson/contracts"
 
-        #raise Exception()
-
-    def test_correct(self,args):
-
-
-        pyramify = PyRamify(verbosity=args.verbosity, draw_svg=args.draw_svg)
-
-
- 
- 
-        [self.rules, self.ruleTraceCheckers, backwardPatterns2Rules, backwardPatternsComplete, self.matchRulePatterns,
-         self.ruleCombinators, self.overlapping_rules, self.subsumption, self.loopingRuleSubsumption] = \
-            pyramify.ramify_directory("ExFamToPerson/transformation/", self.transformation)
- 
-        set_supertypes(self.superclasses_dict, self.rules, self.transformation, self.ruleTraceCheckers, self.matchRulePatterns, self.ruleCombinators)
-
-
-        print("superclasses_dict:")
-        for mm in self.superclasses_dict:
-            print(mm + " : " + str(self.superclasses_dict[mm]))
-
-#load the contracts
-
-        contracts = load_directory("ExFamToPerson/contracts")
-
-        atomic_contracts = [
+        self.atomic_contracts = [
             "Neg_CityCompany",
             "Neg_CountryCity",
             "Neg_SchoolOrdFac",
@@ -109,80 +66,22 @@ class Test:
             "Pos_TownHallComm"
         ]
 
-        if_then_contracts = []
-        prop_if_then_contracts = []
+        self.if_then_contracts = []
 
-        self.atomic_contracts, self.if_then_contracts = load_contracts(contracts, self.superclasses_dict,
-                                                                       atomic_contracts, if_then_contracts,
-                                                                       prop_if_then_contracts,
-                                                                       args.draw_svg)
+        self.prop_if_then_contracts = []
 
-        slicer = Slicer(self.rules, self.transformation, self.superclasses_dict, self.overlapping_rules)
 
-        if args.slice > -1:
-            contract, self.atomic_contracts, self.if_then_contracts = slicer.get_contract(args.slice, self.atomic_contracts, self.if_then_contracts)
-            self.rules, self.transformation = slicer.slice_transformation(contract)
+        #=========PC SAVE LOCATION
 
-        #raise Exception()
- 
-        s = PathConditionGenerator(self.transformation, "ExFamToPerson/Persons_Extended.ecore", self.ruleCombinators, self.ruleTraceCheckers, self.matchRulePatterns, self.overlapping_rules, self.subsumption, self.loopingRuleSubsumption, args)#
-        #raise Exception()
+        self.pc_save_filename = "pcs_atlTrans_extended.txt"
 
-        if not args.pc_filename is None:
-            if len(args.pc_filename) == 0:
-                loaded_pc_dict = load_pcs("pcs_atlTrans_extended.txt")
-            else:
-                loaded_pc_dict = load_pcs(args.pc_filename)
-            s.load_saved_pcs(loaded_pc_dict)
-
-            pc_time = "(PCs loaded)"
-        else:
-
-            ts0 = time.time()
-            s.build_path_conditions()
-            ts1 = time.time()
-
-            pc_time = ts1 - ts0
-            print("\n\nTime to build the set of path conditions: " + str(pc_time))
-
-            save_pcs(s, "pcs_atlTrans_extended.txt")
-
-        print("Number of path conditions: " + str(s.num_path_conditions))
-
-            #print("printing path conditions")
-        #s.print_path_conditions_screen()
-# 
-#        s.print_path_conditions_file()
-        #raise Exception()
-
-        print("\nContract proving:")
-
-        s.verbosity = 0
-
-        contract_prover = ContractProver(slicer)
-
-        contract_prover.prove_contracts(s, self.atomic_contracts, [])#self.if_then_contracts)
-        print("\n\nTime to build the set of path conditions: " + str(pc_time))
-        #        print("Size of the set of path conditions: " + str(float(sys.getsizeof(s.pathConditionSet) / 1024)))
-        print("Number of path conditions: " + str(s.num_path_conditions))
-
-def _print_states(self, s):
-    for state in s.symbStateSpace:
-        print("----------")
-        if state == ():
-            print('Empty')
-        else:
-            for s in state:
-                print(s.name)
 
 
 if __name__ == "__main__":
     parser = load_parser()
     args = parser.parse_args()
 
-
-    exFam = Test()
-    exFam.setUp(args)
+    exFam = ATLTest()
     exFam.test_correct(args)
 
     
---
+++ util/test_script_base.py
@@ -0,0 +1,115 @@
+import time
+import sys
+
+from path_condition_generator import PathConditionGenerator
+
+from pyramify.PyRamify import PyRamify
+
+from util.test_script_utils import get_sub_and_super_classes, set_supertypes, load_contracts, save_pcs, load_pcs, load_transformation
+from util.slicer import Slicer
+
+from core.himesis_utils import load_directory
+
+from PropertyVerification.ContractProver import ContractProver
+
+
+class Test:
+
+    def __init__(self):
+        self.transformation_directory = ""
+        self.contract_directory = ""
+
+        self.full_transformation = []
+        self.rules = []
+        self.transformation = []
+
+        self.ruleTraceCheckers = {}
+        self.matchRulePatterns = {}
+        self.ruleCombinators = {}
+        self.overlapping_rules = {}
+        self.subsumption = {}
+        self.loopingRuleSubsumption = {}
+
+        self.subclasses_dict = {}
+        self.superclasses_dict = {}
+
+        self.atomic_contracts = []
+        self.if_then_contracts = []
+        self.prop_if_then_contracts = []
+
+        self.inputMM = ""
+        self.outputMM = ""
+
+        self.pc_save_filename = ""
+
+    def test_correct(self, args):
+        self.rules, self.transformation = load_transformation(self.transformation_directory, self.full_transformation)
+
+
+        pyramify = PyRamify(verbosity = args.verbosity, draw_svg = args.draw_svg)
+
+        [self.rules, self.ruleTraceCheckers, backwardPatterns2Rules, backwardPatternsComplete, self.matchRulePatterns,
+         self.ruleCombinators, self.overlapping_rules, self.subsumption, self.loopingRuleSubsumption] = \
+            pyramify.ramify_directory(self.transformation_directory, self.transformation)
+
+        self.subclasses_dict, self.superclasses_dict = get_sub_and_super_classes(self.inputMM, self.outputMM)
+
+        set_supertypes(self.superclasses_dict, self.rules, self.transformation, self.ruleTraceCheckers,
+                       self.matchRulePatterns, self.ruleCombinators)
+
+        # load the contracts
+
+        contracts = load_directory(self.contract_directory)
+
+        self.atomic_contracts, self.if_then_contracts = load_contracts(contracts, self.superclasses_dict,
+                                                                       self.atomic_contracts, self.if_then_contracts,
+                                                                       self.prop_if_then_contracts,
+                                                                       args.draw_svg)
+
+        slicer = Slicer(self.rules, self.transformation, self.superclasses_dict, self.overlapping_rules)
+
+        if args.slice > -1:
+            contract, self.atomic_contracts, self.if_then_contracts = slicer.get_contract(args.slice,
+                                                                                          self.atomic_contracts,
+                                                                                          self.if_then_contracts)
+            self.rules, self.transformation = slicer.slice_transformation(contract)
+
+        # raise Exception()
+
+        s = PathConditionGenerator(self.transformation, self.outputMM, self.ruleCombinators, self.ruleTraceCheckers,
+                                   self.matchRulePatterns, self.overlapping_rules, self.subsumption,
+                                   self.loopingRuleSubsumption, args)  #
+        # raise Exception()
+
+        if not args.pc_filename is None:
+            if len(args.pc_filename) == 0:
+                loaded_pc_dict = load_pcs(self.pc_save_filename)
+            else:
+                loaded_pc_dict = load_pcs(args.pc_filename)
+            s.load_saved_pcs(loaded_pc_dict)
+
+            pc_time = "(PCs loaded)"
+        else:
+            ts0 = time.time()
+            s.build_path_conditions()
+            ts1 = time.time()
+
+            pc_time = ts1 - ts0
+            print("\n\nTime to build the set of path conditions: " + str(pc_time))
+
+            save_pcs(s, self.pc_save_filename)
+
+        print("Number of path conditions: " + str(s.num_path_conditions))
+
+        # print("printing path conditions")
+        # s.print_path_conditions_screen()
+        #
+        #        s.print_path_conditions_file()
+        # raise Exception()
+
+        print("\nContract proving:")
+        s.verbosity = 0
+        contract_prover = ContractProver(slicer)
+        contract_prover.prove_contracts(s, self.atomic_contracts, [])  # self.if_then_contracts)
+        print("\n\nTime to build the set of path conditions: " + str(pc_time))
+        print("Number of path conditions: " + str(s.num_path_conditions))
