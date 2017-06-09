9fc67f6453d995813132561dbcb348d8e53b4df9
Bentley James Oakes <bentleyjoakes@gmail.com>
2017-06-09 15:39:06 -0400

    Unify test scripts into one script.

---
+++ collect_results.py
@@ -0,0 +1,66 @@
+import subprocess
+
+def get_avg(l):
+    return sum(l) / float(len(l))
+
+times_to_run = 2
+
+experiments = ["test_atlTrans_extended"]
+
+for ex_file in experiments:
+
+    ex_filename = ex_file + "_results.txt"
+    with open(ex_filename, 'w') as g:
+
+        for x in range(times_to_run):
+
+            print("Running " + ex_file + " for time " + str(x))
+            proc = subprocess.Popen(['/usr/bin/time', 'python3', ex_file + ".py"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            out, err = proc.communicate()
+
+            if out:
+                for line in out:
+                    g.write(line)
+            if err:
+                for line in err:
+                    g.write(line)
+
+
+    with open(ex_filename) as f:
+
+        time_build_pcs = []
+        time_contract_proof = []
+        memory = []
+        num_pcs = []
+
+
+        for line in f:
+
+            if "Time to build the set of path conditions" in line:
+                t = line.split(" ")[8]
+                time_build_pcs.append(float(t))
+            elif "seconds to prove" in line:
+                t = line.split(" ")[1]
+                time_contract_proof.append(float(t))
+            elif "maxresident" in line:
+                line = line.replace("maxresident)k", "")
+                m = line.split(" ")[5]
+                memory.append(int(m)/1000)
+            elif "Number of path conditions:" in line:
+                pcs = line.split(" ")[4]
+                num_pcs.append(int(pcs))
+
+            #print(line.strip())
+
+
+        avg_build_time = get_avg(time_build_pcs)
+        avg_proof_time = get_avg(time_contract_proof)
+
+        print(" ")
+        print("Time build: " + str(round(avg_build_time, 2)))
+        print("Time proof: " + str(round(avg_proof_time, 2)))
+        print("Memory: " + str(int(get_avg(memory))))
+        print("Number of path conditions: " + str(int(get_avg(num_pcs))))
+
+
+
--- collect_results_mbeddr.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-echo "Testing test_mbeddr.py" > results_mbeddr.txt
-for i in {1..15}
-do
-   echo -e "\nNumber of rules: $i" >> results_mbeddr.txt
-   /usr/bin/time python2 test_mbeddr.py --no_svg --num_rules=$i  2>&1 | tail -4 >> results_mbeddr.txt
-done
