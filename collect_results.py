84346ac3d3e76fab00017b32196f93ee60172ac8
Bentley James Oakes <bentleyjoakes@gmail.com>
2018-03-22 16:15:25 -0400

    Try numpy variance.

--- collect_results.py
+++ collect_results.py
@@ -1,8 +1,11 @@
 import subprocess
 import os
 import shutil
-import statistics.variance as variance
-
+try:
+    import statistics.variance as variance
+except ImportError:
+    import numpy.var as variance
+    
 def sizeof_fmt(num, suffix='B'):
     for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
         if abs(num) < 1024.0:
